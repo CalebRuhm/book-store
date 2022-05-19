8bbe2306ab4958d4ee4bc934e23698deff774f09
Caleb Ruhm <ruhmc0619@gmail.com>
2022-05-18 21:55:58 -0400

    routes

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,11 +1,27 @@
 import "../Styles/App.scss";
-import Navbar from "../Components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "../Components/NavBar";
+import Login from "../Components/Login";
+import BookShelf from "../Components/BookShelf";
+import Search from "../Components/Search";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="App">
+      <NavBar />
+      <div className="Content">
+        <Routes>
+          <Route exact path="/" element={<Login />} component={Login} />
+          <Route
+            exact
+            path="/book-shelf"
+            element={<BookShelf />}
+            component={BookShelf}
+          />
+          <Route path="/search" element={<Search />} component={Search} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
