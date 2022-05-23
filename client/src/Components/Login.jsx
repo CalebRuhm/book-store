d0e01e9a18bb703bea6eacde81985b47d018a204
Caleb Ruhm <ruhmc0619@gmail.com>
2022-05-23 16:52:59 -0400

    login scss

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,9 +2,24 @@ import "../Styles/Login.scss";
 import React from "react";
 
 export default function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
-      <button>Login</button>
+      <form
+        className="form"
+        onsubmit={(e) => {
+          onSubmit(e);
+        }}
+      >
+        <label for="username">Username:</label>
+        <input type="text" name="username"></input>
+        <label for="password">Password:</label>
+        <input type="password" name="password"></input>
+      </form>
+      <button className="btn">Login</button>
     </div>
   );
 }
