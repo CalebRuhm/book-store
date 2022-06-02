c20fc639b699ac31a0712336c224beeebb34a626
Caleb Ruhm <ruhmc0619@gmail.com>
2022-06-02 17:41:45 -0400

    search errors

--- client/src/Stores/searchStore.jsx
+++ client/src/Stores/searchStore.jsx
@@ -8,11 +8,19 @@ export const searchStore = create((set) => ({
     set({ searchData: e.target.value });
   },
   handleSearch: async (searchData) => {
-    const response = await axios(`/api/book/search/${searchData}`, {
-      method: "GET",
-    });
-    set({
-      response: response.data.books,
-    });
+    if (!searchData) {
+      alert("Please enter a book title");
+    } else {
+      try {
+        const response = await axios(`/api/book/search/${searchData}`, {
+          method: "GET",
+        });
+        set({
+          response: response.data.books,
+        });
+      } catch (error) {
+        alert("We're sorry! An unexpected error occurred");
+      }
+    }
   },
 }));
